fcb18bf7a953c7486c064cc5513041467fd34c64
Loren Cipriano <loren25cipriano@gmail.com>
2020-12-14 14:09:28 -0800

    commit tree 6c0035351e1bf4d88e814966b97ac5b8ac2176be

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,17 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+
+doc.css(".title-oE5vT4") #=> Change Things 
+    #=> <h1 class="headline-26OIBN">Change things</h1>
+# p doc.css(".headline-26OIBN")  #=> [#<Nokogiri::XML::Element:0x3fe13ac3fd10 name="h1" attributes=[#<Nokogiri::XML::Attr:0x3fe13ac3fc98 name="class" value="headline-26OIBN">] 
+    #children=[#<Nokogiri::XML::Text:0x3fe13ac3f838 "Change things">]>]
+
+
+courses = doc.css(".title-oE5vT4")
+
+courses.each do |course|
+    puts course.text.strip
+end 
+
